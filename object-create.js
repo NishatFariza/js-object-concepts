108584bb1bfeb5cdba4ec595841400c0d17d80b1
NishatFariza <nishatfarzia78@gmail.com>
2022-02-26 04:01:59 +0600

    different way to create object

--- object-create.js
+++ object-create.js
@@ -1,1 +1,29 @@
+//using literal
 const student ={name:'Sakib-al-Hasan', job:'crckter'}
+
+//using constractor
+const person =new Object();
+
+//3
+// const human = object.create(student);
+// console.log(human.job);
+
+//4. null object
+// const huma =object.create(null);
+
+//5. creating object from class
+class People {
+    constructor(name){
+        this.name = name;
+        this.age =age;
+    }
+}
+const peop =new People('manush', 12);
+console.log(peop);
+
+//5. syntactically sugar
+function Manus (name){
+    this.name =name;
+}
+const man =new Manus('Kader')
+console.log(man);
